d3a225b38b5537e5d0a313ad361f1b9a99da6095
Fathi Mohammed Saeed AlQadasi <fathi733@gmail.com>
2023-05-03 09:58:00 +0300

    Create sdf1.py
    add file

---
+++ sdf1.py
@@ -0,0 +1,33 @@
+
+import sqlite3
+
+# Connect to the database
+conn = sqlite3.connect('employee.db')
+c = conn.cursor()
+
+# Create the employee table
+c.execute('''CREATE TABLE IF NOT EXISTS employee (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                leaves INTEGER,
+                absent INTEGER
+            )''')
+
+# Get user input
+employee_id = input("Enter the employee id: ")
+employee_name = input("Enter the name of employee")
+leaves = input("Enter the number of leaves: ")
+absent = input("Enter the number of absent days: ")
+
+# Update the employee name 
+
+# Update the employee's leaves and absent days
+c.execute(f"UPDATE employee SET leaves = {leaves}, absent = {absent} WHERE id = {employee_id}")
+
+# Get all employees
+c.execute("SELECT * FROM employee")
+print(c.fetchall())
+
+# Commit changes and close the connection
+conn.commit()
+conn.close()
