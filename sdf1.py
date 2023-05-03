34e52afaa8b27b75e94073743c88dd136bd62c66
Fathi Mohammed Saeed AlQadasi <fathi733@gmail.com>
2023-05-03 11:02:22 +0300

    Update sdf1.py
    update

--- sdf1.py
+++ sdf1.py
@@ -13,16 +13,30 @@ c.execute('''CREATE TABLE IF NOT EXISTS employee (
             )''')
 
 # Get user input
-employee_id = input("Enter the employee id: ")
-name = input("Enter the name of employee: ")
-leaves = input("Enter the number of leaves: ")
-absent = input("Enter the number of absent days: ")
+action = input("Enter 'insert', 'update', or 'remove': ")
 
-# Add new employee name 
-c.execute(f"INSERT INTO employee (name, leaves, absent) VALUES ('{name}', {leaves}, {absent})")
+if action == 'insert':
+    # Get user input
+    name = input("Enter the employee name: ")
+    leaves = input("Enter the number of leaves: ")
+    absent = input("Enter the number of absent days: ")
 
-# Update the employee's leaves and absent days
-c.execute(f"UPDATE employee SET leaves = {leaves}, absent = {absent} WHERE id = {employee_id}")
+    # Add a new employee
+    c.execute(f"INSERT INTO employee (name, leaves, absent) VALUES ('{name}', {leaves}, {absent})")
+elif action == 'update':
+    # Get user input
+    employee_id = input("Enter the employee id: ")
+    leaves = input("Enter the number of leaves: ")
+    absent = input("Enter the number of absent days: ")
+
+    # Update the employee's leaves and absent days
+    c.execute(f"UPDATE employee SET leaves = {leaves}, absent = {absent} WHERE id = {employee_id}")
+elif action == 'remove':
+    # Get user input
+    employee_id = input("Enter the id of the employee you want to remove: ")
+
+    # Remove the employee
+    c.execute(f"DELETE FROM employee WHERE id = {employee_id}")
 
 # Get all employees
 c.execute("SELECT * FROM employee")
